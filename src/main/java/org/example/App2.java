84fe4c28d1da4b2cb9ae180b70b113d26199117f
postoenko114-coder <postoenko114@gmail.com>
2025-10-24 13:49:59 +0200

    Доработать наш аналог GSON так, чтобы он умел
    1) Работать с полями типа массив
    2) Работать с вложенными объектами, например:
    
    class X { }
    
    class Y {
      String name;
      X x; // !!!
    }

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -1,10 +1,40 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+
+class Z {
+    int length;
+    int height;
+
+    Z(int length, int height) {
+        this.length = length;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
+
 
 class Person {
     int name = 111;
     String surname = "Hello";
+    int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Z z = new Z(1, 2);
 }
 
 public class App2 {
@@ -20,20 +50,111 @@ public class App2 {
 
         for (Field field : fields) {
             field.setAccessible(true);
-            Object value = field.get(person);
+            Class<?> type = field.getType();
 
-            sb.append('"')
-                    .append(field.getName())
-                    .append('"')
-                    .append(": ")
-                    .append('"')
-                    .append(value)
-                    .append('"').append(',');
+            boolean isObject = !type.isPrimitive();
+            boolean isCustomObject =
+                    isObject &&
+                            !type.getPackageName().startsWith("java.") &&
+                            !type.isArray();
+            if (field.getType().isArray()) {
 
+                sb.append('"')
+                        .append(field.getName())
+                        .append('"')
+                        .append(": ")
+                        .append('[');
+                Object value = field.get(person);
+                int length = Array.getLength(value);
+                for (int i = 0; i < length; i++) {
+                    Object element = Array.get(value, i);
+                    sb.append('"')
+                            .append(element)
+                            .append('"')
+                            .append(",");
+
+                }
+                if (sb.length() > 1) {
+                    sb.setLength(sb.length() - 1);
+                }
+                sb.append(']');
+            } else if (isCustomObject) {
+                Object nestedObj = field.get(person);
+
+                Class<?> cls1 = nestedObj.getClass();
+
+                sb.append('"')
+                        .append(cls1.getName())
+                        .append('"')
+                        .append(": ")
+                        .append('{');
+
+                Field[] fields1 = cls1.getDeclaredFields();
+                for (Field field1 : fields1) {
+                    field1.setAccessible(true);
+
+                    if (field1.getType().isArray()) {
+
+                        sb.append('"')
+                                .append(field1.getName())
+                                .append('"')
+                                .append(": ")
+                                .append('[');
+                        Object value = field.get(person);
+                        int length = Array.getLength(value);
+                        for (int i = 0; i < length; i++) {
+                            Object element = Array.get(value, i);
+                            sb.append('"')
+                                    .append(element)
+                                    .append('"')
+                                    .append(",");
+
+                        }
+                        if (sb.length() > 1) {
+                            sb.setLength(sb.length() - 1);
+                        }
+                        sb.append(']');
+                    } else {
+                        Object value = field1.get(nestedObj);
+
+                        sb.append('"')
+                                .append(field1.getName())
+                                .append('"')
+                                .append(": ")
+                                .append('"')
+                                .append(value)
+                                .append('"').append(',');
+
+                    }
+                }
+                if (sb.length() > 1) {
+                    sb.setLength(sb.length() - 1);
+                }
+                    sb.append('}');
+                }else{
+
+                    Object value = field.get(person);
+
+                    sb.append('"')
+                            .append(field.getName())
+                            .append('"')
+                            .append(": ")
+                            .append('"')
+                            .append(value)
+                            .append('"').append(',');
+                }
+            sb.append(',');
+
+
+
+        }
+        if (sb.length() > 1) {
+            sb.setLength(sb.length() - 1);
         }
 
         sb.append("}");
 
         System.out.println(sb.toString());
     }
+
 }
